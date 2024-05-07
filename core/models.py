7339063f18f5de85d49f036cc2d0b3cf76e8d797
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-05-07 11:11:32 +0300

    some changes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class ShortenedURL(models.Model):
+    long_url = models.URLField()
+    short_code = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = self.generate_unique_short_code()
+        super().save(*args, **kwargs)
+
+    def generate_unique_short_code(self):
+        return str(uuid.uuid4())[:8]
+
+    def __str__(self):
+        return f"{self.short_code} => {self.long_url}"
--- shorty/settings.py
+++ shorty/settings.py
@@ -110,11 +110,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
